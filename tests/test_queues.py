b6ffc050fa46a2d2943e44535a5f834efbf9e41c
Sean <seansilva003@gmail.com>
2025-03-20 20:00:02 +1100

    Add array_stack and circular_queue

--- tests/test_queues.py
+++ tests/test_queues.py
@@ -1,6 +1,91 @@
 from unittest import TestCase
 
 from data_structures.linked_queue import LinkedQueue
+from data_structures.circular_queue import CircularQueue
+
+class TestCircularQueue(TestCase):
+    EMPTY = 0
+    ROOMY = 5
+    LARGE = 10
+    CAPACITY = 20
+
+    def setUp(self) -> None:
+        self.lengths = [self.EMPTY, self.ROOMY, self.LARGE, self.ROOMY, self.LARGE]
+        self.queues = [CircularQueue(self.CAPACITY) for i in range(len(self.lengths))]
+        for queue, length in zip(self.queues, self.lengths):
+            for i in range(length):
+                queue.append(i)
+        self.empty_queue = self.queues[0]
+        self.roomy_queue = self.queues[1]
+        self.large_queue = self.queues[2]
+        #we build empty queues from clear.
+        #this is an indirect way of testing if clear works!
+        #(perhaps not the best)
+        self.clear_queue = self.queues[3]
+        self.clear_queue.clear()
+        self.lengths[3] = 0
+        self.queues[4].clear()
+        self.lengths[4] = 0
+
+    def tearDown(self) -> None:
+        for s in self.queues:
+            s.clear()
+
+    def test_init(self) -> None:
+        self.assertTrue(self.empty_queue.is_empty())
+        self.assertEqual(len(self.empty_queue), 0)
+
+    def test_len(self) -> None:
+        """ Tests the length of all queues created during setup."""
+        for queue, length in zip(self.queues, self.lengths):
+            self.assertEqual(len(queue), length)
+
+    def test_is_empty_add(self) -> None:
+        """ Tests queues that have been created empty/non-empty."""
+        self.assertTrue(self.empty_queue.is_empty())
+        self.assertFalse(self.roomy_queue.is_empty())
+        self.assertFalse(self.large_queue.is_empty())
+
+    def test_is_empty_clear(self) -> None:
+        """ Tests queues that have been cleared."""
+        for queue in self.queues:
+            queue.clear()
+            self.assertTrue(queue.is_empty())
+
+    def test_is_empty_serve(self) -> None:
+        """ Tests queues that have been served completely."""
+        for queue in self.queues:
+            #we empty the queue
+            try:
+                while True:
+                    was_empty = queue.is_empty()
+                    queue.serve()
+                    #if we have served without raising an assertion,
+                    #then the queue was not empty.
+                    self.assertFalse(was_empty)
+            except:
+                self.assertTrue(queue.is_empty())
+
+    def test_is_full_add(self) -> None:
+        """ Tests queues that have been created not full."""
+        self.assertFalse(self.empty_queue.is_full())
+        self.assertFalse(self.roomy_queue.is_full())
+        self.assertFalse(self.large_queue.is_full())
+
+    def test_append_and_serve(self) -> None:
+        for queue in self.queues:
+            nitems = self.ROOMY
+            for i in range(nitems):
+                queue.append(i)
+            for i in range(nitems):
+                self.assertEqual(queue.serve(), i)
+
+    def test_clear(self) -> None:
+        for queue in self.queues:
+            queue.clear()
+            self.assertEqual(len(queue), 0)
+            self.assertTrue(queue.is_empty())
+
 
 
 class TestLinkedQueue(TestCase):
--- tests/test_stacks.py
+++ tests/test_stacks.py
@@ -1,6 +1,91 @@
 from unittest import TestCase
 
+from data_structures.array_stack import ArrayStack
 from data_structures.linked_stack import LinkedStack
+
+class TestStack(TestCase):
+    EMPTY = 0
+    ROOMY = 5
+    LARGE = 10
+    CAPACITY = 20
+
+    def setUp(self) -> None:
+        self.lengths = [self.EMPTY, self.ROOMY, self.LARGE, self.ROOMY, self.LARGE]
+        self.stacks = [ArrayStack(self.CAPACITY) for i in range(len(self.lengths))]
+        for stack, length in zip(self.stacks, self.lengths):
+            for i in range(length):
+                stack.push(i)
+        self.empty_stack = self.stacks[0]
+        self.roomy_stack = self.stacks[1]
+        self.large_stack = self.stacks[2]
+        #we build empty stacks from clear.
+        #this is an indirect way of testing if clear works!
+        #(perhaps not the best)
+        self.clear_stack = self.stacks[3]
+        self.clear_stack.clear()
+        self.lengths[3] = 0
+        self.stacks[4].clear()
+        self.lengths[4] = 0
+
+    def tearDown(self) -> None:
+        for s in self.stacks:
+            s.clear()
+
+    def test_init(self) -> None:
+        self.assertTrue(self.empty_stack.is_empty())
+        self.assertEqual(len(self.empty_stack), 0)
+
+    def test_len(self) -> None:
+        """ Tests the length of all stacks created during setup."""
+        for stack, length in zip(self.stacks, self.lengths):
+            self.assertEqual(len(stack), length)
+
+    def test_is_empty_add(self) -> None:
+        """ Tests stacks that have been created empty/non-empty."""
+        self.assertTrue(self.empty_stack.is_empty())
+        self.assertFalse(self.roomy_stack.is_empty())
+        self.assertFalse(self.large_stack.is_empty())
+
+    def test_is_empty_clear(self) -> None:
+        """ Tests stacks that have been cleared."""
+        for stack in self.stacks:
+            stack.clear()
+            self.assertTrue(stack.is_empty())
+
+    def test_is_empty_pop(self) -> None:
+        """ Tests stacks that have been popped completely."""
+        for stack in self.stacks:
+            #we empty the stack
+            try:
+                while True:
+                    was_empty = stack.is_empty()
+                    stack.pop()
+                    #if we have popped without raising an assertion,
+                    #then the stack was not empty.
+                    self.assertFalse(was_empty)
+            except:
+                self.assertTrue(stack.is_empty())
+
+    def test_is_full_add(self) -> None:
+        """ Tests stacks that have been created not full."""
+        self.assertFalse(self.empty_stack.is_full())
+        self.assertFalse(self.roomy_stack.is_full())
+        self.assertFalse(self.large_stack.is_full())
+
+    def test_push_and_pop(self) -> None:
+        for stack in self.stacks:
+            nitems = self.ROOMY
+            for i in range(nitems):
+                stack.push(i)
+            for i in range(nitems-1, -1, -1):
+                self.assertEqual(stack.pop(), i)
+
+    def test_clear(self) -> None:
+        for stack in self.stacks:
+            stack.clear()
+            self.assertEqual(len(stack), 0)
+            self.assertTrue(stack.is_empty())
+
 
 
 class TestLinkedStack(TestCase):
